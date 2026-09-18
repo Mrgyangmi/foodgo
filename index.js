e01e73f9b2cb060c0ee429ddec45068d59bd197e
HEMAN <HEMAN@HEMANs-MacBook-Pro.local>
2026-09-19 02:33:45 +0900

    Prepare FoodGo for deployment

--- index.js
+++ index.js
@@ -936,7 +936,8 @@ app.patch("/orders/:id/status", (req, res) => {
     res.send("Order status updated successfully!");
   });
 });
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on http://localhost:3000");
   console.log("MySQL connected!");
 });
